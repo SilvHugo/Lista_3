8721487ab2bcf46cfb15dc466af3cf32c0532c65
Hugo <hugogusot@gmail.com>
2025-07-22 09:50:54 -0400

    Todas as atividades finalizadas

---
+++ Exercício 7/app.js
@@ -0,0 +1,9 @@
+const menu = document.getElementById('menu')
+
+function clickMenu( ){
+    if(menu.style.display === 'block')
+        menu.style.display = 'none'
+    else
+        menu.style.display = 'block'
+
+}
